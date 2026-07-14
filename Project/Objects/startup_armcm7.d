6dfb071c6268f4d1a2f77c328390b1ee65f20f04
SL8620 <2584032673@qq.com>
2026-07-14 19:30:15 +0800

    初始化，工程文件不带x的才可以修改时钟

---
+++ Project/Objects/startup_armcm7.d
@@ -0,0 +1,4 @@
+./objects/startup_armcm7.o: RTE\Device\ARMCM7\startup_ARMCM7.c \
+  C:\Users\SL8620\AppData\Local\Arm\Packs\ARM\Cortex_DFP\1.1.0\Device\ARMCM7\Include\ARMCM7.h \
+  C:\Users\SL8620\AppData\Local\Arm\Packs\ARM\CMSIS\6.3.0\CMSIS\Core\Include\core_cm7.h \
+  C:\Users\SL8620\AppData\Local\Arm\Packs\ARM\Cortex_DFP\1.1.0\Device\ARMCM7\Include\system_ARMCM7.h
